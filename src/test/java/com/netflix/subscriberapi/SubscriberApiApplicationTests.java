c8fc645fd3d348222dc63fa1a38f1b4968ff7e14
Glen Fannin <devglen@icloud.com>
2021-05-20 08:21:57 -0400

    Add files via upload

---
+++ src/test/java/com/netflix/subscriberapi/SubscriberApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.netflix.subscriberapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SubscriberApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
