fc97bdff169080b59157978f7477d67c63c22f13
Glen Fannin <devglen@icloud.com>
2021-05-20 21:00:49 -0400

    Updates and cleanup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,20 @@ CREATE TABLE subscriptions (
   created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   last_modified_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+DROP TABLE IF EXISTS credit_card_number_formats;
+
+CREATE TABLE credit_card_number_format_rules (
+    id INT AUTO_INCREMENT  PRIMARY KEY,
+    card_network VARCHAR(100) NOT NULL,
+    iin_range VARCHAR(255) NOT NULL,
+    length_range VARCHAR(50) NOT NULL
+);
+
+INSERT INTO credit_card_number_format_rules
+(card_network,iin_range,length_range)
+VALUES
+('AMEX','[34,37]','[15]'),
+('MASTERCARD','[51,52,53,54,55,222100-272099]','[16]'),
+('VISA','[4]','[13-19]'),
+('DISCOVER','[6011,622126-622925,644,645,646,647,648,649,65]','[16-19]');
